fa0e2974c52ad775b076214e7a48185e831398c6
SelfMadeSystem <sms@shoghisimon.ca>
2026-01-01 12:17:58 -0500

    Add note about expected WebAssembly JavaScript Promise Integration support in major browsers by 2026

--- src/pyodide/warnJSPI.py
+++ src/pyodide/warnJSPI.py
@@ -3,4 +3,7 @@ print("""\
 Warning: Your browser does not support WebAssembly JavaScript Promise Integration.
 Debugging will not work and interactive input with the integrated terminal is unavailable.
 To use these features, please switch to a browser that supports this feature, such as the latest version of \x1b[1mGoogle Chrome\x1b[22m or \x1b[1mMicrosoft Edge\x1b[22m.
-If using Firefox, enable the \x1b[1mjavascript.options.wasm_js_promise_integration\x1b[22m setting in \x1b[1mabout:config\x1b[22m.\x1b[0m""")
+If using \x1b[1mFirefox\x1b[22m, enable the \x1b[1mjavascript.options.wasm_js_promise_integration\x1b[22m setting in \x1b[1mabout:config\x1b[22m.\x1b[0m""")
+print("""\
+\x1b[36m\
+Note: It is expected that all major browsers will implement WebAssembly JavaScript Promise Integration by the end of 2026.\x1b[0m""")
